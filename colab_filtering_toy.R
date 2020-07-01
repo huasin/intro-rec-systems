d816935d97f89600164e107ef029fa0d9cc78428
huasin <elias.alegria@ug.uchile.cl>
2020-06-30 22:07:30 -0400

    presentacion

--- colab_filtering_toy.R
+++ colab_filtering_toy.R
@@ -5,7 +5,7 @@ library(purrr)
 
 # Leer la data ------------------------------------------------------------
 
-df <- read.csv("datasets/input/toy_dataset.csv", row.names = 1)
+df <- read.csv("datasets/toy_dataset.csv", row.names = 1)
 
 # Reemplazo na's por ceros
 df[is.na(df)] <- 0
@@ -25,18 +25,19 @@ df <- df %>%
 
 # Matriz de similitud de item ---------------------------------------------
 
-cos.sim <- function(mat){
-  sim <- mat/sqrt(rowSums(mat*mat))
-  sim <- sim %*% t(sim)
-  return(sim)
-}
+# cos.sim <- function(mat){
+#   sim <- mat/sqrt(rowSums(mat*mat))
+#   sim <- sim %*% t(sim)
+#   return(sim)
+# }
+# item_sim <- cos.sim(as.matrix(t(df))) %>% as.data.frame()
 
-item_sim <- cos.sim(as.matrix(t(df))) %>% as.data.frame()
+item_sim <- cor(df) %>% as.data.frame()
 
 # Funcion para calcular pelicula cercana ----------------------------------
 
 get_similar_movies <- function(movie_name, user_rating) {
-  similar_score <- item_sim[movie_name]*(user_rating-2.5)
+  similar_score <- item_sim[movie_name]*(user_rating)
   # similar_score <- similar_score[order(similar_score, decreasing = T),, drop = F]
   return(similar_score)
 }
@@ -51,17 +52,3 @@ user_ratings <- c(5,1,1)
 user_similar_movies <- map2_dfc(user_movies, user_ratings, get_similar_movies)
 rowSums(user_similar_movies) %>% sort(decreasing = T)
 
-
-
-# recommenderlab ----------------------------------------------------------
-library(recommenderlab)
-
-m <- as.matrix(df)
-m <- as(m, "realRatingMatrix")
-
-getRatingMatrix(m)
-
-rec_jac <- Recommender(m, method = "IBCF", param = list(method = "Jaccard"))
-rec_cos <- Recommender(m, method = "IBCF", param = list(method = "Cosine"))
-
-predict(rec_cos, m[4], n = 2) %>% as("list")
